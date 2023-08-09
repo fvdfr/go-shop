39d6509e84f33e0b894a6d5153eadd74f0b768b8
huhwibxh <2946665756@qq.com>
2023-08-09 17:22:22 +0800

    feat(new):新增

---
+++ shop-app-admin/src/views/layout/Sider/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.less";
+const Logo = () => {
+  return (
+    <div className="sidebar-logo-container">
+      {/* <img src={''} className="sidebar-logo" alt="logo" /> */}
+      <h1 className="sidebar-title">shop-admin</h1>
+    </div>
+  );
+};
+
+export default Logo;
